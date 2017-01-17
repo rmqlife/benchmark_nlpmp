e593f25ce1390269d9799b383147d362b39d0f2d
rmqlife <rmqlife@gmail.com>
2017-01-16 20:23:10 -0600

    add laptop

--- src/benchmark_laptop.cpp
+++ src/benchmark_laptop.cpp
@@ -10,6 +10,39 @@
 #include<ros/console.h>
 
 using namespace std;
+
+visualization_msgs::Marker load_marker(ros::NodeHandle &nh, std::string name){
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "map"; // "map" is our global coordinate system
+    marker.header.stamp = ros::Time::now();
+
+    marker.ns = name;
+    marker.id = 0;
+
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.type = visualization_msgs::Marker::CUBE;
+    marker.lifetime = ros::Duration();
+
+    std::vector<double> pos,size,color;
+    nh.getParam(name+"/position",pos);
+    nh.getParam(name+"/size",size);
+    nh.getParam(name+"/color",color);
+    
+    marker.color.r = color[0] / 255.;
+    marker.color.g = color[1] / 255.;
+    marker.color.b = color[2] / 255.;
+    marker.color.a = 1.;
+    marker.pose.position.x = pos[0];
+    marker.pose.position.y = pos[1];
+    marker.pose.position.z = pos[2];
+    marker.scale.x = size[0];
+    marker.scale.y = size[1];
+    marker.scale.z = size[2];   
+    
+    return marker;
+}
+
+
 int main(int argc, char** argv)
 {
     setbuf(stdout, NULL);
@@ -27,35 +60,10 @@ int main(int argc, char** argv)
 
     // visualization marker array
     visualization_msgs::MarkerArray marker_array;
-    visualization_msgs::Marker marker;
 
-    marker.header.frame_id = "map"; // "map" is our global coordinate system
-    marker.header.stamp = ros::Time::now();
+    marker_array.markers.push_back(load_marker(nh,"table"));
+    marker_array.markers.push_back(load_marker(nh,"laptop"));
 
-    marker.ns = "table";
-    marker.id = 0;
-
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.type = visualization_msgs::Marker::CUBE;
-    marker.lifetime = ros::Duration();
-
-    marker.color.r = 139. / 255.;
-    marker.color.g = 69. / 255.;
-    marker.color.b = 19. / 255.;
-    marker.color.a = 1.;
-    
-    std::vector<double> pos,size;
-    nh.getParam("table/position",pos);
-    nh.getParam("table/size",size);
-    
-    marker.pose.position.x = pos[0];
-    marker.pose.position.y = pos[1];
-    marker.pose.position.z = pos[2];
-    marker.scale.x = size[0];
-    marker.scale.y = size[1];
-    marker.scale.z = size[2];    
-
-    marker_array.markers.push_back(marker);
 
     // Run main loop
     ros::Rate rate(1);
